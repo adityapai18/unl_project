7a5443a3d06275ac816a322092c80156377430e9
Aditya Pai <adityaajitpai@gmail.com>
2022-11-12 18:25:15 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+import UnlSdk from "unl-map-js";
+const map = new UnlSdk.Map({
+  apiKey: "biuV9WvyeSBKpj2sKcNrQklQbIcy8bbm",
+  vpmId: "02ff70f8-88ad-42cd-be58-9af7e7574cb9",
+  gridControl: true,
+  indoorMapsControl: true,
+  tilesSelectorControl: true,
+  draftShapesControl: true,
+  container: "map",
+  center: [0, 0],
+  zoom: 1,
+  minZoom: 2,
+});
